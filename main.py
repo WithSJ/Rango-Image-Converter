953cb0b525380fdd9798d672d758b04b4012a1b7
Sandeep Jadam <withsj.in@gmail.com>
2021-05-13 22:36:06 +0530

    added console interface ppm and png convertor.

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ args = parser.parse_args()
 
 # End script console interface.
 
-from image_converter.reader import read_ppm
-from image_converter.writer import write_png
+from image_converter.reader import *
+from image_converter.writer import *
 
 IMG = list()
 WIDTH = int()
@@ -26,7 +26,14 @@ HEIGHT = int()
 
 if(args.Input.split('.')[1] in ("ppm","PPM")):
     WIDTH, HEIGHT, IMG = read_ppm(args.Input)
+
+elif(args.Input.split('.')[1] in ("png","PNG")):
+    WIDTH, HEIGHT, IMG,info = read_png(args.Input)
              
 if args.Output.split(".")[1] in ("png","PNG"):
     write_png(args.Output,WIDTH,HEIGHT,IMG)
+
+elif args.Output.split(".")[1] in ("ppm","PPM"):
+    write_ppm(args.Output,WIDTH,HEIGHT,IMG)
+
 
